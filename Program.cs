e6e3ddcb7206b33c87adeb738cfc8bc8f064c0d6
M <M@DESKTOP-52HSJNQ>
2023-04-22 12:14:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов в массиве");
+            int arraySize = int.Parse(Console.ReadLine());
+            int[] arr = new int[arraySize];
+            Random rnd = new Random();
+
+            Console.WriteLine("Первоначальный массив:");
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                arr[i] = rnd.Next(100);
+                Console.Write(arr[i] + " ");
+            }
+            Shuffle(arr);
+
+            Console.WriteLine("\nПеремешанный массив:");
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Shuffle(int[] arr)
+        {
+            Random rng = new Random();
+
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                int j = rng.Next(i + 1);
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+
+        }
+    }
+}
